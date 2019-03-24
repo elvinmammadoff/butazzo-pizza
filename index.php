aae4c276addfe862d006d709a254c7d53c2eee20
Elvin <elvinm@crocusoft.com>
2019-03-24 16:55:41 +0400

    register form sent mail

--- index.php
+++ index.php
@@ -732,7 +732,8 @@
 					$headers = 'From: webmaster@example.com' . "\r\n" .
 						'Reply-To: webmaster@example.com' . "\r\n" .
 						'X-Mailer: PHP/' . phpversion();
-					mail($to, $subject, $message, $headers);					
+                    mail($to, $subject, $message, $headers);
+                    echo "Failed to send.";					
 				}
 			}
 		?>	
