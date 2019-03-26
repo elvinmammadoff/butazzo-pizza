5027653bb8de7a7b05da2c5f6470cf3cb025bde7
Elvin <elvinm@crocusoft.com>
2019-03-27 00:56:08 +0400

    mailgun config

--- index.php
+++ index.php
@@ -719,18 +719,7 @@
                 # Instantiate the client.
                 $mgClient = new Mailgun('036bcbf18d89fc6bec6237815877c1ca-e51d0a44-89eb75f0');   
                 $ip = '104.130.122.30';  
-                
-                # Issue the call to the client.
-                $result = $mgClient->get("ips/$ip");   
-                $result = $mgClient->post("domains", array(
-                    "total_count" => 1,
-                    "smtp_login" => "postmaster@sandbox70e67361a1ec4a3daa70d81c37eae905.mailgun.org",
-                    "name" => "smtp.mailgun.org",
-                    "smtp_password" => "fc11801a21d69627a3a3d593e6f7f788-e51d0a44-c8d5ff23",
-                    "wildcard" => true,
-                    "spam_action" => "disabled",
-                    "state" => "active"
-                ));                   
+                                  
                 
                 // First, instantiate the SDK with your API credentials
                 $mg = Mailgun::create('036bcbf18d89fc6bec6237815877c1ca-e51d0a44-89eb75f0'); // For US servers
@@ -743,7 +732,21 @@
                 'to'      => 'elvinmammadoff@gmail.com',
                 'subject' => 'The PHP SDK is awesome!',
                 'text'    => 'It is so simple to send a message.'
-                ]);                
+                ]);     
+
+                # Issue the call to the client.
+                $result = $mgClient->get("ips/$ip");   
+                $result = $mgClient->post("domains", array(
+                    "total_count" => 1,
+                    "smtp_login" => "postmaster@sandbox70e67361a1ec4a3daa70d81c37eae905.mailgun.org",
+                    "name" => "smtp.mailgun.org",
+                    "smtp_password" => "fc11801a21d69627a3a3d593e6f7f788-e51d0a44-c8d5ff23",
+                    "wildcard" => true,
+                    "spam_action" => "disabled",
+                    "state" => "active"
+                ));   
+                
+                var_dump($result);
 				
 				if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
 				   $error_message = "Invalid email format"; 
