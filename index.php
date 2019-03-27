a295f10f78edc48c8b2bcee00e890fed566b1503
Elvin <elvinm@crocusoft.com>
2019-03-28 01:24:36 +0400

    mailgun config

--- index.php
+++ index.php
@@ -47,33 +47,6 @@
 <!-- BEGIN body -->
 
 <body data-spy="scroll" data-target=".navbar" data-offset="50">
-
-<?php
-
-require 'vendor/autoload.php';
-use Mailgun\Mailgun;
-
-$error_message = '';
-if(isset($_POST["send"])){
-
-    $name = htmlspecialchars($_POST["name"]);
-    $email = htmlspecialchars($_POST["email"]);
-    $phone = htmlspecialchars($_POST["phone"]);
-    $date = htmlspecialchars($_POST["date"]);
-    $time = htmlspecialchars($_POST["time"]);
-    $message = htmlspecialchars($_POST["message"]);
-    
-    $mg = new Mailgun('d720e6fa4b9634b558dcb5863bfbb4f7-47317c98-faa6435b'); // For US servers
-    $domain = 'sandbox63a4e019e8bb4d7cb2666133ae923151.mailgun.org';
-
-    $result = $mg->sendMessage('sandbox63a4e019e8bb4d7cb2666133ae923151.mailgun.org', [
-        'from'    => $email,
-        'to'      => 'metinzakirov@gmail.com',
-        'subject' => $message,
-        'text'    => 'It is so simple to send a message.'
-    ]);
-}
-?>
   
     <!-- BEGIN  Loading Section -->  
     <div class="loading-overlay">
@@ -735,7 +708,7 @@ if(isset($_POST["send"])){
                             <h1 class="section-title title_sty1">online reservation</h1>
 							<?= $error_message?>
                             <p class="short">Lorem Ipsum is simply dummy text of the printing and typesetting industry. </p>
-                            <form id="reserv_form" method="post">
+                            <form id="reserv_form" method="post" action="form.php">
                                 <div class="row">
                                     <div class="col-sm-6">
                                         <div class="form-group form_pos">
